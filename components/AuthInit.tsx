711df1ee6af8b8e3e94adc33ac0cf961e3729d78
serkan0266 <s.becerikliii@gmail.com>
2026-04-16 02:13:37 +0200

    fix: import supabase path

--- components/AuthInit.tsx
+++ components/AuthInit.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "@/lib/supabase";
 
 export default function AuthInit() {
   useEffect(() => {
