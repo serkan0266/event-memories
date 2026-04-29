e12d0ad65dab434e4e5745704ac1d9babb979a7e
serkan0266 <s.becerikliii@gmail.com>
2026-04-29 20:36:08 +0200

    fix header image not showing (allow supabase images)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["cbenqgkrucmsbsnhxaww.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+    ],
   },
 };
 
